70f11bdd2b8337520bfed4a2a20804cd34be7893
SirOlash <Olasupoemmanuel30@gmail.com>
2024-10-29 09:30:03 +0100

    Assignment

--- JavaAssignment/FullMultiplicationTable.java
+++ JavaAssignment/FullMultiplicationTable.java
@@ -1,17 +1,11 @@
 public class FullMultiplicationTable{
-public static void main(String[] args){
-
-int i=1;
-
-while (i < 13 ){
-	int table = 1 * i;
-	System.out.println(1 + " * " + i + " = " + table);	
-	i++;}
-
-while (i < 13 ){
-	int table = 2 * i;
-	System.out.println(2 + " * " + i + " = " + table);	
-	i++;}
-
-}
+   public static void main(String [] args){
+	for(int multiple = 1; multiple < 10; multiple++){
+		for(int times = 1; times < 10; times++){
+			int product = multiple * times;
+			System.out.printf("%d * %d = %d\t",times,multiple,product);
+			}
+			System.out.println();
+		}
+	}
 }

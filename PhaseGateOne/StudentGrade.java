189e557edac2af30cd34e348312af54c6309a340
SirOlash <Olasupoemmanuel30@gmail.com>
2024-12-03 11:59:19 +0100

    first-hour

--- PhaseGateOne/StudentGrade.java
+++ PhaseGateOne/StudentGrade.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StudentGrade{
  public static void main(String... args){
 	Scanner input = new Scanner(System.in);
+
+	  ArrayList <Integer> subject = new ArrayList<Integer>();
 
 	
 	
@@ -17,21 +20,40 @@ public class StudentGrade{
 	System.out.println();
 
 	for(int student = 1; student <= amountOfStudents; student++){
-		for(int subject = 1;subject <= subjectOffered;subject++ ){
+		for(int subjects = 1;subjects <= subjectOffered;subjects++ ){
 			System.out.println("Entering score for student " + student);
-			System.out.print("Enter score for subject " + subject+ " : ");
+			System.out.print("Enter score for subject " + subjects+ " : ");
 			int studentScore = input.nextInt();
+			subject.add(studentScore);
+
 			System.out.println("Saving >>>>>>>>>>>>>>>>>>>");
 			System.out.println("Saved successfully!!!");
+			System.out.println();
 	
 		}
-
-
 	}
-	
+
+	displayScore(amountOfStudents, subjectOffered);
 
 
 
  }
+
+	public static void displayScore(int amountOfStudents, int subjectOffered){
+		System.out.println("========================================================");
+	System.out.print("STUDENT\t");
+		for(int counter = 1; counter <= subjectOffered; counter++){  
+			System.out.print("SUB"+ counter + "\t");
+		}		
+	System.out.println("TOTAL");
+
+	System.out.println("========================================================");
+
+
+
+	}
+
+
+
 
 } 

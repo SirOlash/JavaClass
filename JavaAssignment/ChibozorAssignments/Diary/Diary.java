bef42cfdbc3669cad8969505b9ac34c08e68c0e0
SirOlash <Olasupoemmanuel30@gmail.com>
2025-01-07 13:41:01 +0100

    Diary

--- JavaAssignment/ChibozorAssignments/Diary/Diary.java
+++ JavaAssignment/ChibozorAssignments/Diary/Diary.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Diary{
-	private ArrayList<DiaryEntry> entries;
+	private final ArrayList<DiaryEntry> entries;
 	private Boolean isLocked;
+	private String password;
+	Scanner input = new Scanner(System.in);
+	
 
 	public Diary(){
 		entries = new ArrayList<>();
-		isLocked = false;
+		isLocked = true;
 	}
 
 	public void createNewEntry(String id, String content){
@@ -19,21 +23,46 @@ public class Diary{
 		else{System.out.print("Your diary is Locked, unlock diary to add entry!!!");}
 		
 	}
-	
-	public void createDiary() {
-       		System.out.println("new diary created");
-    	}
 
 	public void unlockDiary() {
-        	isLocked = false;
-        	System.out.println("dairy has been unlocked");
+		
+		System.out.println("Enter your password!!");
+		String checkPassword = input.nextLine();
+		if (password != null && password.equals(checkPassword)){
+        		isLocked = false;
+        		System.out.println("dairy has been unlocked");}
+		else{System.out.print("Password is either incorrect or You have to Create a new password");}
    	}
     
 	public void lockDiary() {
         	isLocked = true;
         	System.out.println("dairy has been locked");
+		
     	}
-
+	public void createPassword(){
+		if (password == null){
+			System.out.println("Create a password");
+			String password = input.nextLine();
+			//password = password.trim();
+			this.password = password.trim();
+			//System.out.printf("Password is %s",password);
+			System.out.println("Password has been created");
+		}
+		else{System.out.println("There is already a password created. Change the password");}
+	}
+	
+	public void changePassword() {
+		if (password != null){
+			System.out.println("Enter Your previous password");
+			String oldPassword = input.nextLine();
+			if (oldPassword.equalsIgnoreCase(password)){
+				System.out.println("Create a password");
+                this.password = input.nextLine();
+			}
+			else{System.out.print("Wrong Password!!!!");}
+		}
+	}
+	
 	public DiaryEntry findEntryById(String id){
 		for (DiaryEntry entry: entries){
 			if (entry.getId().equalsIgnoreCase(id)){
@@ -42,6 +71,7 @@ public class Diary{
 		}
 		return null;
 	}
+	
 	
 	public void updateEntry(String id, String content){
 		DiaryEntry entry = findEntryById(id);
@@ -66,7 +96,7 @@ public class Diary{
         	if (entry != null && !isLocked) {
             		System.out.printf("entry %s: %s", entry.getId(), entry.getContent());
 		} 
-		else {System.out.println("entry is null or diary is locked");}
+		else {System.out.println("entry does not exist or diary is locked");}
 	}
 
 	
--- JavaAssignment/ChibozorAssignments/Diary/DiaryEntry.java
+++ JavaAssignment/ChibozorAssignments/Diary/DiaryEntry.java
@@ -1,12 +1,11 @@
 public class DiaryEntry{
 	
-	private String id;
+	private final String id;
 	private String content;
 
 	public DiaryEntry(String id,String content){
 		this.id = id;
 		this.content = content;
-
 	}
 	public void setContent(String content){
 		this.content = content;
